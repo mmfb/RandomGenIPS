3e26c6466770d4937f44a0a8cc08111c9c5940a7
unknown <miguel.bugalho@gmail.com>
2019-10-24 16:38:55 +0100

    Added two rules to define limits to the random number

--- models/randomNumber.js
+++ models/randomNumber.js
@@ -1,6 +1,22 @@
 
 
 module.exports.genRandomNumber =
-  function () {
-      return Math.random();
+  function (num1, num2) {
+    console.log("Model :"+num1+" "+num2+
+    " "+(typeof num1 == 'number'
+    && typeof num2== 'number'));
+   
+      if (typeof num1 == 'number'
+         && typeof num2== 'number') {
+        if (num1 > num2) {
+            var aux = num1;
+            num1 = num2;
+            num2 = aux;
+        }
+        return Math.random() * (num2 - num1)  + num1;
+      } else if (typeof num1 == 'number') {
+          return Math.random() * num1;
+      } else {
+         return Math.random();
+     }
   }
--- routes/randomNumberRoute.js
+++ routes/randomNumberRoute.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
     res.send({ number:randomNumberGen.genRandomNumber()});
 });
 
+
+router.get('/:top', function(req, res, next) {
+    res.send({ number:randomNumberGen.
+        genRandomNumber(parseFloat(req.params.top))});
+});
+
+router.get('/:min/:max', function(req, res, next) {
+    console.log(req.params.min+" "+req.params.max)
+    res.send({ number:randomNumberGen.
+        genRandomNumber(parseFloat(req.params.min),
+        parseFloat(req.params.max))});
+});
 module.exports = router;
